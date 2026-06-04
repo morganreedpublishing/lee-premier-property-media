2691730ed94d16c244cee8375bae9d8f8817700d
Greg <morganreedpublishing@gmail.com>
2026-06-04 09:27:31 -0400

    Rename aerial coverage package

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -2,7 +2,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const PACKAGES = {
   "Essential Package": 22500,
-  "Premier Photo + Drone": 37500,
+  "Essential Package + Aerial Coverage": 37500,
   "Luxury Media Bundle": 65000,
 };
 
